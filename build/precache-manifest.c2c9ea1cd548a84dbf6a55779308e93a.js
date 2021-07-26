e09dcd8a9755e2b9e29ccb538539eed02da44dd5
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2021-07-26 21:19:18 +0530

    After hosting

---
+++ build/precache-manifest.c2c9ea1cd548a84dbf6a55779308e93a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc81d1002c87aee188cba3206efb942",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e58e04e3989709d2cdea",
+    "url": "/static/css/main.cd4a23a9.chunk.css"
+  },
+  {
+    "revision": "cae355c25a4c6eeb1890",
+    "url": "/static/js/2.ee204092.chunk.js"
+  },
+  {
+    "revision": "6f001b22a9cb1fcb0d750f6c56d216df",
+    "url": "/static/js/2.ee204092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e58e04e3989709d2cdea",
+    "url": "/static/js/main.873d819b.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
