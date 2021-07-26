2789778980189a3fd501da9c8c2c2368946ca9bd
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2021-07-26 19:52:23 +0530

    Added the popup form feature

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import {
+  Container,
+  Grow,
+  Grid,
+  Button,
+  Dialog,
+  DialogContent,
+} from "@material-ui/core";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import AddCircleIcon from "@material-ui/icons/AddCircle";
 import { useDispatch } from "react-redux";
 
 import { getPosts } from "../../actions/posts";
@@ -8,11 +17,20 @@ import Form from "../Form/Form";
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0);
+  const [openPopup, setOpenPopup] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
+
+  const handleClickOpen = () => {
+    setOpenPopup(true);
+  };
+
+  const handleClickClose = () => {
+    setOpenPopup(false);
+  };
 
   return (
     <Grow in>
@@ -32,9 +50,25 @@ const Home = () => {
             <Posts setCurrentId={setCurrentId} />
           </Grid>
           <Grid item xs={12} sm={4}>
-            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
+            <Button onClick={handleClickOpen}>
+              <AddCircleIcon
+                style={{ transform: "scale(1.8)" }}
+                fontSize="large"
+              />
+            </Button>
           </Grid>
         </Grid>
+
+        <Dialog open={openPopup}>
+          {/* <DialogTitle>Create Yout</DialogTitle> */}
+          <DialogContent>
+            <Button onClick={handleClickClose}>
+              <HighlightOffIcon />
+            </Button>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </DialogContent>
+        </Dialog>
       </Container>
     </Grow>
   );
